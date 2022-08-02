80b5a22f48578e50f0ead27237a28d178a552191
Sergey Shlegel <serg-69@mail.ru>
2022-08-02 19:01:19 +0300

    added new files

---
+++ for.py
@@ -0,0 +1,13 @@
+students = ["serg", "alex", "tonya", "olga", "sveta", "kolya"]
+# for f in students:
+#     if f == "olga":
+#         var = "engeneer " + f
+#         print(var)
+# for f in students[:3]:  # цикл вывода первых трех элементов списка
+#     print(f)
+# for f in students[3:]:  # цикл вывода трех последних элементов списка
+#     print(f)
+# for f in students[1:4]:  # вывод элементов списка с первого по третий включительно
+#     print(f)
+for f in students:
+    print(len(f))  # вывод количества символов в каждом элементе

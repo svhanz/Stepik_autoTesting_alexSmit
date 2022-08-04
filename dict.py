5f5ffe6d864a6e4e1f7176acd6606715bbfd7b69
Sergey Shlegel <serg-69@mail.ru>
2022-08-04 13:36:40 +0300

    added dict.py работа со словарем

---
+++ dict.py
@@ -0,0 +1,10 @@
+# список
+# famaly = ["serg", "olya", "max", "dasha", "serg"]
+# print(famaly)  # выведет ['serg', 'olya', 'max', 'dasha', 'serg']
+# # множества
+# famaly_1 = {"serg", "olya", "max", "dasha", "serg"}
+# print(famaly_1)  # выведет {'olya', 'dasha', 'serg', 'max'}
+# словарь (ключ: значение)
+family = {"dad": "serg", "mom": "olya", "son": "max", "daughter": "dasha"}
+for k, v in family.items():
+    print(k)  # выведет dad mom son daughter (в столбик)

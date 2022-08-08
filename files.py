f79b4ea9c1496579a5a81534ee3d10280245cca3
Sergey Shlegel <serg-69@mail.ru>
2022-08-08 14:48:53 +0300

    learn write info to file

--- files.py
+++ files.py
@@ -1,5 +1,14 @@
 # создаем файл в директории doc. а- специальный мод
-fw = open("doc/file.txt", "a")
-fw.write("hello world\n")  # записываем file.txt строку hello world
-fw.write("hi, serg\n")
-fw.close()
+from asyncore import write
+
+
+# fw = open("doc/file.txt", "a")
+# fw.write("hello world\n")  # записываем file.txt строку hello world
+# fw.write("hi, serg\n")
+# fw.close()
+# a- запись новых данных в файл и помещение их в конец файла
+# w- запись новых данных, но с удалением старых данных
+var = input("Напиши что-нибудь: ")
+fw = open("doc/file_1.txt", "a")
+fw.write(var)
+fw.close(var)
